1d90b0d27582442ae91a1139ca090b5803585be6
Mark Szymczyk <mark@meandmark.com>
2020-11-19 22:09:32 -0500

    Create an initial version of the iOS web view.

--- iOS/WebView.swift
+++ iOS/WebView.swift
@@ -5,4 +5,22 @@
 //  Created by mark on 11/16/20.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var html: String
+    
+    init(html: String) {
+        self.html = html
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.loadHTMLString(html, baseURL: nil)
+    }
+}
