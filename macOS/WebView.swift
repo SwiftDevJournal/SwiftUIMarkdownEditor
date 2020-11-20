00527c718b42665709769606cc45481a52509762
Mark Szymczyk <mark@meandmark.com>
2020-11-19 20:39:07 -0500

    Create a shell of the Mac web view from Trozware's Back to the Mac SwiftUI video.

--- macOS/WebView.swift
+++ macOS/WebView.swift
@@ -5,4 +5,22 @@
 //  Created by mark on 11/19/20.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: NSViewRepresentable {
+    var html: String
+    
+    init(html: String) {
+        self.html = html
+    }
+    
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        nsView.loadHTMLString(html, baseURL: nil)
+    }
+}
