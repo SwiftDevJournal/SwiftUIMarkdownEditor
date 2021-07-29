cad55a09c93f2a5771d96e3507f33bd091d1672a
Mark Szymczyk <mark@swiftdevjournal.com>
2021-07-29 14:47:58 -0400

    Add the docco theme to the available themes to use. All of the themes that comes with CodeEditor are dark themes. I wanted a light theme to be available.

--- Shared/CodeThemeNames.swift
+++ Shared/CodeThemeNames.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+import CodeEditor
+
+// The CodeEditor SwiftUI view package uses the Highlightr package to do the syntax highlighting. The Highlightr packages has dozens of themes, but the CodeEditor package includes only 6 of the themes.  Use this extension to add any Highlightr themes you want to the editor.
+public extension CodeEditor.ThemeName {
+    static var docco = CodeEditor.ThemeName(rawValue: "docco")
+}
